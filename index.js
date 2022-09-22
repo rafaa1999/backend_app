c8b66d0324f8a64e5f55f60fe4f3fc2982f7e61c
rafaa1999 <raferhouma10@gmail.com>
2022-09-22 19:44:04 +0100

    cross_env & config

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 const express=require("express")
 const UserService= require("./src/user/UserService")
-
-
 const sequelize=require("./src/config/database")
 const app = require("./src/app")
 
-sequelize.sync({force:true}).then(async ()=>{
-    for(let i=1;i<=5;i++){
-        const user={
-            username:`user${i}`,
-            email:`user${i}@gmail.com`,
-            password:`pass07`
+    // choose the environment
+    if(process.env.NODE_ENV === "production"){
+        sequelize.sync()
+    }else{
 
-        }
-       await UserService.create(user)
+        sequelize.sync({force:true}).then(async ()=>{
+            for(let i=1;i<=5;i++){
+                const user={
+                    username:`user${i}`,
+                    email:`user${i}@gmail.com`,
+                    password:`pass07`
+        
+                }
+            await UserService.create(user)
+            }
+        })
     }
-})
+
+
 
 
 app.listen(3000,()=>{
-    console.log("server is running")
+    console.log("app is running in mode:",process.env.NODE_ENV)
 })
 
 
--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,46 @@
 const { Sequelize } = require('sequelize');
 
+// init the configuration for 
+// each mod-env
+    // let configuration={
+    //   db:'test-db',
+    //   username:'user',
+    //   password:'pass',
+    //   dialect: 'sqlite',
+    //   host: './dev.sqlite',
+    //   logging:true
+    // }
+
+    // // choose the env
+    // if(process.env.NODE_ENV === "production"){
+    //   configuration={
+    //     db:'prod-db',
+    //     username:'prod-user',
+    //     password:'prod-pass',
+    //     dialect: 'sqlite',
+    //     host: './prod.sqlite',
+    //     logging:false
+    //   }
+    // }
+const config = require("config")
+// get the data form the config (json format)
+const configuration=config.get("database") // we choose the right env depend on the env that start in the execution
+
+
+
 // create instance to connect to
 // database locally
-const sequelize = new Sequelize("test-db","user","pass",{
-  dialect: 'sqlite',
+const sequelize = new Sequelize(configuration.db,configuration.username,configuration.password,{
+  dialect: configuration.dialect,
   // keep the data on a file
-  host: './dev.sqlite'
+  host: configuration.host,
   // we can run database on memory
   // mode (in the RAM) hence every
   // time the app running the data
   // will go on
   // used for test environment
  // host: ':memory:'
+ logging:configuration.logging
 });
 
 
